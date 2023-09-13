a6d30607688191818c8f646e61bbfbb4d14e4a45
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2023-09-14 02:17:56 +0300

    Added file-saver and some content

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-about',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+  downloadCV() {
+    const pdfUrl = '../assets/cv.pdf';
+    const pdfName = 'Silviu-Barbu-CV.pdf'; 
 
+    fetch(pdfUrl)
+      .then((response) => response.blob())
+      .then((blob) => {
+        saveAs(blob, pdfName);
+      })
+      .catch((error) => {
+        console.error('Error downloading CV:', error);
+      });
+  }
 }
