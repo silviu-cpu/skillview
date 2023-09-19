74f64d3aa7c9da2f8686ccceec5ad736bc79c24f
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2023-09-19 16:21:49 +0300

    finished

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { saveAs } from 'file-saver';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements AfterViewInit {
+  typed: Typed | undefined;
+ 
+
+  ngAfterViewInit(): void {
+    const options = {
+      strings: ['a software engineer' , 'a web developer' , 'a frontend developer'],
+      typeSpeed: 50, // typing speed in milliseconds
+      backSpeed: 25, // backspacing speed in milliseconds
+      loop: true // loop the animation
+    };
+
+    this.typed = new Typed('.typing-text', options);
+  }
+
   downloadCV() {
     const pdfUrl = '../assets/cv.pdf';
     const pdfName = 'Silviu-Barbu-CV.pdf'; 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  typed: Typed | undefined;
 
+  ngAfterViewInit(): void {
+    const options = {
+      strings: ['Silviu' , 'Skillview' ],
+      typeSpeed: 50, // typing speed in milliseconds
+      backSpeed: 25, // backspacing speed in milliseconds
+      loop: true // loop the animation
+    };
+
+    this.typed = new Typed('.typing-text', options);
+  }
 }
